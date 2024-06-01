8f8d098ffc713f36090940a88047f1f953861dd3
Vicky-peace <victorpeace47@gmail.com>
2024-06-01 15:28:47 +0300

    solved some conflicts

--- client/Jobster/src/App.tsx
+++ client/Jobster/src/App.tsx
@@ -1,41 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Landing from './pages/Landing'
+import './App.css';
+import Landing from './pages/Landing';
+import Dashboard from './pages/Dashboard/Dashboard';
 import Register from './pages/Register';
 import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   return (
-    <>
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-      <Route path="/" element={<Landing/>} />
-      <Route path="/register" element={<Register/>} />
-
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-
     </Router>
-    
+  );
+};
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Landing from './pages/Landing'
-import Dashboard from './pages/Dashboard'
-import Register from './pages/Register'
-function App() {
-  return (
-    <>
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/register' element={<Register />} />
-
-     </Routes>
-     </BrowserRouter>
-    </>
-  )
-}
-
-export default App
+export default App;
