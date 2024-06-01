592555e273ff424a37f247e308022f472b429704
Vicky-peace <victorpeace47@gmail.com>
2024-06-01 12:07:07 +0300

    added navbar

--- client/Jobster/src/App.tsx
+++ client/Jobster/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Landing from './pages/Landing'
 import Register from './pages/Register';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
   return (
     <>
     <Router>
+      <Navbar/>
       <Routes>
       <Route path="/" element={<Landing/>} />
       <Route path="/register" element={<Register/>} />
