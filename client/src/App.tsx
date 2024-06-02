712af81b77e59b6f5997e480c8fd14efee03cc97
Vicky-peace <victorpeace47@gmail.com>
2024-06-02 20:52:07 +0300

    solved some bugs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,32 @@
 import React, { useEffect, useState } from 'react';
-
-import { BrowserRouter as Router, Route, Routes , useLocation} from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Landing from './pages/Landing';
-import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
-  const [showNavbar, setShowNavBar] = useState(true);
-
+  const [showNavbar, setShowNavbar] = useState(true);
   const location = useLocation();
 
-
-  useEffect(() =>{
-    if(location.pathname === '/' || location.pathname === 'register'){
-      setShowNavBar(false);
-    }else {
-      setShowNavBar(true);
+  useEffect(() => {
+    if (location.pathname === '/' || location.pathname === '/register') {
+      setShowNavbar(false);
+    } else {
+      setShowNavbar(true);
     }
   }, [location.pathname]);
 
-  
   return (
-    <Router>
-     {showNavbar  && <Navbar />} 
-
+    <div>
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 
